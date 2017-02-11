d46a3ab1f1e568e829c8804c773b82f11ce56e45
Aidan Hallett <ahallett@uk.ibm.com>
2017-02-11 22:03:05 +0000

    Updated to click and select date from calander

--- epoch.js
+++ epoch.js
@@ -7,41 +7,54 @@ function unixTimeLoad() {
     var utcTime = getDate.toUTCString().slice(0, -4);
     $("#utcTimeClock").html(utcTime);
 
-  var stopClock = false;
-  $(".clockDiv").mouseover(function () {
-    stopClock = true;
-    console.log('mouse in');
-    $(".clockDiv").css('backgroundColor', '#dadafc');
-  });
-  $(".clockDiv").mouseout(function () {
-    stopClock = false;
+    var stopClock = false;
+    $(".clockDiv").mouseover(function () {
+        stopClock = true;
+        console.log('mouse in');
+        $(".clockDiv").css('backgroundColor', '#dadafc');
+    });
+    $(".clockDiv").mouseout(function () {
+        stopClock = false;
 
-    console.log('mouse out');
-    $(".clockDiv").css('backgroundColor', '#F5F5F5');
-  });
-  setInterval(function () {
-    if (!stopClock) {
-      var getDate = new Date();
-      var unixTime = Math.floor(getDate.getTime() / 1000.0);
-      $("#unixTimeClock").html(unixTime);
-      var utcTime = getDate.toUTCString().slice(0, -4);
-      $("#utcTimeClock").html(utcTime);
+        console.log('mouse out');
+        $(".clockDiv").css('backgroundColor', '#F5F5F5');
+    });
+    setInterval(function () {
+        if (!stopClock) {
+            var getDate = new Date();
+            var unixTime = Math.floor(getDate.getTime() / 1000.0);
+            $("#unixTimeClock").html(unixTime);
+            var utcTime = getDate.toUTCString().slice(0, -4);
+            $("#utcTimeClock").html(utcTime);
 
 
-    }
-  }, 1000);
+        }
+    }, 1000);
 
-  $('.convertToDateString').click(function(){
-    console.log()
-  });
+    $('#utcInput').daterangepicker({
+        "singleDatePicker": true,
+        "timePicker": true,
+        "timePicker24Hour": true,
+        locale: {
+            format: 'MM/DD/YYYY HH:mm'
+        }
+    });
+
+    $('#utcInput').change(function(){
+        var utcInput = $("#utcInput").val();
+        console.log(utcInput);
+        var unixNumeric = Math.round((new Date(utcInput).getTime())/ 1000);
+
+        $("#unixResult").html(unixNumeric);
+    });
 
 }
 
 
-function unixToUTC(){
+function unixToUTC() {
     console.log('click to convert');
 
-    var unixtime = $("#unixInput").val()*1000;
+    var unixtime = $("#unixInput").val() * 1000;
     console.log(unixtime);
     var utcString = new Date(unixtime).toUTCString().slice(0, -4);
     console.log(utcString);
@@ -50,8 +63,3 @@ function unixToUTC(){
 
 }
 
-function utcToUNIX(){
-    var utcInput = $("#utcInput").val();
-    var unixNumeric = Math.round(new Date(utcInput).getTime()/1000);
-    $("#unixResult").html(unixNumeric);
-}
